63254a70559024f6bf10440a3f927921be037359
Mietek Przytuła <mieczyslawp.js@gmail.com>
2020-11-28 02:00:36 +0100

    Updates

---
+++ precache-manifest.1f2ce2cb10cf096e5d5d85a11189c7f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e8761376662abbff046",
+    "url": "/static/css/main.83aa3405.chunk.css"
+  },
+  {
+    "revision": "7e8761376662abbff046",
+    "url": "/static/js/main.7e876137.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e12795637c47cc4e6448",
+    "url": "/static/js/2.e1279563.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "85cf5d55cd681c03da26bb113c8bf768",
+    "url": "/index.html"
+  }
+];
